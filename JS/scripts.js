b4ccc1b793677ad70a1ce06af1f96e01cdd09e89
Bruno <faithkidbruno@gmail.com>
2022-05-17 16:29:23 -0300

    implementei funções no menu

---
+++ JS/scripts.js
@@ -0,0 +1,23 @@
+// usar um dom para mudar a imagem sempre que o botão for pressionado
+
+let div = document.getElementById('fotoAtual');
+
+let n = 1;
+div.style.backgroundImage = `url(../CSS/assets/gymEquip/${n}.jpg)`;
+
+function fotoAnterior() {
+    n -= 1;
+    if (n < 1)
+        n = 6;
+    div.style.backgroundImage = `url(../CSS/assets/gymEquip/${n}.jpg)`;
+
+}
+
+function proximaFoto() {
+    n += 1;
+    if (n > 6)
+        n = 1;
+    div.style.backgroundImage = `url(../CSS/assets/gymEquip/${n}.jpg)`;
+
+}
+
